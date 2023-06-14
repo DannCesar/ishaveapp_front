e2d2afd3ecc3e1851bbf08c317d2cac56f5132ed
Daniel Cesar Soares <danielcesartds@gmail.com>
2023-06-14 12:59:27 -0300

    Feat: props Layout

--- src/pages/Scheduling/index.tsx
+++ src/pages/Scheduling/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Layout } from "../../components/Layout";
-import * as S from "./styles"
+import * as S from "./styles";
+import { Button } from "../../components/Button";
 
 export const Scheduling: React.FC = () => {
   return (
     <>
       <Layout>
         <S.Container>
-          
+          <div className="oi">
+          </div>
+          <Button></Button>
         </S.Container>
-        
       </Layout>
     </>
   );
--- src/pages/Scheduling/styles.ts
+++ src/pages/Scheduling/styles.ts
@@ -2,7 +2,14 @@ import styled from "styled-components";
 
 
 export const Container = styled.div`
-width: 200px;
-height: 200px;
-background-color: #fff;
+width: 400px;
+height: 400px;
+display: flex;
+background-color: aliceblue;
+
+    & .oi{
+        h1 {
+            color: #fff;
+        }
+    }
 `
