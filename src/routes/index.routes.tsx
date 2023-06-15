83b53b49d3ac79406bad869584d93d1dc2193dd2
Daniel Cesar Soares <danielcesartds@gmail.com>
2023-06-15 12:45:44 -0300

    Feat: Page not found

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import { publicRoutes } from "./public.routes"
 import { privateRoutes } from "./private.routes";
 
+//Criação do index para rotas e condição ternária para identificar qual rota exibir
 
 export const Routes = () => {
     const idSession = localStorage.getItem("ishaveappId")
--- src/routes/private.routes.tsx
+++ src/routes/private.routes.tsx
@@ -1,16 +1,21 @@
 import { Layout } from "../components/Layout";
 import { Home } from "../pages/Home";
+import { PageNotFound } from "../pages/PageNotFound";
 import { Scheduling } from "../pages/Scheduling";
 
-
+//Rota privada para quando estiver logado
 
 export const privateRoutes = [
     {
-        path: "/login",
+        path: "/",
         element: <Home/>
     },
     {
         path: "/agendamento",
         element: <Scheduling/>
-    }
+    },
+    {
+        path:"*",
+        element: <PageNotFound/>
+    },
 ]
