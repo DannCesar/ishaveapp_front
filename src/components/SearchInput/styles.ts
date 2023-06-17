da5f0c67d02d4f7a4e1119eb4a06f5d01172bfc1
Daniel Cesar Soares <danielcesartds@gmail.com>
2023-06-17 12:17:25 -0300

    Feat: SearchInput component

---
+++ src/components/SearchInput/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+display: flex;
+height: 20px;
+padding: 4px 8px;
+align-items: center;
+border-radius: 8px;
+gap: 8px;
+
+ & input{
+    display: flex;
+    border: none;
+    font-size: 16px;
+ }
+`;
