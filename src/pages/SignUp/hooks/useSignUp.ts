cfd238d24adae69d6fba8640fd06fae2813b5804
Daniel Cesar Soares <danielcesartds@gmail.com>
2023-06-20 15:17:15 -0300

    Feat: validation

--- src/pages/SignUp/hooks/useSignUp.ts
+++ src/pages/SignUp/hooks/useSignUp.ts
@@ -2,29 +2,34 @@ import { useFormik } from "formik";
 import { UserService } from "../../../services/UserService";
 import { useEffect, useState } from "react";
 import { cnpj } from "cpf-cnpj-validator";
-import * as Yup from "yup";
+import * as yup from "yup";
 
 //Instanciação da classe UserService
 const userApi = new UserService();
 
 // Validação dos campos
-const schema = Yup.object().shape({
-  nomeEmpresa: Yup.string().required("Campo obrigatório"),
-  cnpjEmpresa: Yup.string()
-    .required("Campo obrigatório")
-    .test("oneOfRequired", "Informe um CNPJ válido", (item) => {
-      if (item) return cnpj.isValid(item);
-    }),
-  telEmpresa: Yup.string().required("Campo obrigatório"),
-  emailUsuario: Yup.string().required("Campo obrigatório"),
-  senhaUsuario: Yup.string().required("Campo obrigatório"),
-  cep: Yup.string().required("Campo obrigatório"),
-  logradouro: Yup.string().required("Campo obrigatório"),
-  numEmpresa: Yup.string().required("Campo obrigatório"),
-  bairro: Yup.string().required("Campo obrigatório"),
-  cidade: Yup.string().required("Campo obrigatório"),
-  uf: Yup.string().required("Campo obrigatório"),
-});
+// const schema = yup.object().shape({
+//   nomeEmpresa: yup.string().required("Campo obrigatório"),
+//   cnpjEmpresa: yup
+//     .string()
+//     .required("Campo obrigatório")
+//     .test("oneOfRequired", "Informe um CNPJ válido", (item) => {
+//       if (item) return cnpj.isValid(item);
+//     }),
+//   telEmpresa: yup.string().required("Campo obrigatório"),
+//   emailUsuario: yup.string().required("Campo obrigatório"),
+//   senhaUsuario: yup
+//     .string()
+//     .required("Campo obrigatório")
+//     .min(8, "A senha precisa conter pelo menos 8 caracteres"),
+//   // confirmarSenha: yup.string().required("Campo obrigatório").oneOf([yup.ref("senhaUsuario")], "As senham precisam ser iguais"),
+//   cep: yup.string().required("Campo obrigatório"),
+//   logradouro: yup.string().required("Campo obrigatório"),
+//   numEmpresa: yup.string().required("Campo obrigatório"),
+//   bairro: yup.string().required("Campo obrigatório"),
+//   cidade: yup.string().required("Campo obrigatório"),
+//   uf: yup.string().required("Campo obrigatório"),
+// });
 
 export const useSignUp = () => {
   const [loading, setLoading] = useState(false);
@@ -66,7 +71,7 @@ export const useSignUp = () => {
         );
       }
     },
-    validationSchema: schema,
+    // validationSchema: schema,
   });
   return {
     formSignUp,
