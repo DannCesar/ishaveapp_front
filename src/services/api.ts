bd879bec592a814160e2ced95138eeaa27738c93
IVan-Gfs <ivangfs2004@gmail.com>
2023-06-21 19:32:03 -0300

    verificar se url expirou

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 //Criação da api e exportação para conectar com back-end
 
-const baseURL = 'https://ishaveapi-02087cf5fe26.herokuapp.com/';
-// const baseURL = 'http://localhost:8081' ;
+// const baseURL = 'https://ishaveapi-02087cf5fe26.herokuapp.com/';
+const baseURL = 'http://localhost:5000' ;
 
 export const api = axios.create({
     baseURL
